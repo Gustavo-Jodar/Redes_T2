6c37a9ce3ab9148c4250ce032b3b820d8f4ee971
lucas-cid <lucas_mcid@hotmail.com>
2021-09-18 07:19:26 -0300

    Parte do passo 3

--- tcp.py
+++ tcp.py
@@ -41,12 +41,11 @@ class Servidor:
         if (flags & FLAGS_SYN) == FLAGS_SYN:
             # A flag SYN estar setada significa que é um cliente tentando estabelecer uma conexão nova
             # TODO: talvez você precise passar mais coisas para o construtor de conexão
+            seq = random.randint(1, 9999999)
             conexao = self.conexoes[id_conexao] = Conexao(
-                self, id_conexao, seq_no + 1)
+                self, id_conexao, seq_no + 1, seq + 1)
             # TODO: você precisa fazer o handshake aceitando a conexão. Escolha se você acha melhor
             # fazer aqui mesmo ou dentro da classe Conexao.
-            seq = random.randint(1, 9999999)
-
             header = make_header(new_src_port, new_dst_port, seq,
                                  seq_no + 1, FLAGS_SYN | FLAGS_ACK)
             header = fix_checksum(header, new_src_addr, new_dst_addr)
@@ -62,11 +61,12 @@ class Servidor:
 
 
 class Conexao:
-    def __init__(self, servidor, id_conexao, seq_no):
+    def __init__(self, servidor, id_conexao, seq_no, ack_no):
         self.servidor = servidor
         self.id_conexao = id_conexao
         self.callback = None
         self.seq_no = seq_no
+        self.ack_no = ack_no
         # um timer pode ser criado assim; esta linha é só um exemplo e pode ser removida
         self.timer = asyncio.get_event_loop().call_later(1, self._exemplo_timer)
         # self.timer.cancel()   # é possível cancelar o timer chamando esse método; esta linha é só um exemplo e pode ser removida
@@ -91,7 +91,7 @@ class Conexao:
         new_src_port = dst_port
         new_dst_port = src_port
 
-        header = make_header(new_src_port, new_dst_port, 1,
+        header = make_header(new_src_port, new_dst_port, self.ack_no,
                              self.seq_no, FLAGS_ACK)
         header = fix_checksum(header, new_src_addr, new_dst_addr)
         self.servidor.rede.enviar(header, new_dst_addr)
@@ -115,7 +115,25 @@ class Conexao:
         # TODO: implemente aqui o envio de dados.
         # Chame self.servidor.rede.enviar(segmento, dest_addr) para enviar o segmento
         # que você construir para a camada de rede.
-        pass
+        if(len(dados) > MSS):
+            print("aqui")
+            self.enviar(dados[:MSS])
+            self.enviar(dados[MSS:])
+        else:
+            (src_addr, src_port, dst_addr, dst_port) = self.id_conexao
+
+            new_src_addr = dst_addr
+            new_dst_addr = src_addr
+
+            new_src_port = dst_port
+            new_dst_port = src_port
+
+            header = make_header(new_src_port, new_dst_port, self.ack_no,
+                                 self.seq_no, FLAGS_ACK)
+            header = fix_checksum(header, new_src_addr, new_dst_addr)
+            self.servidor.rede.enviar(header+dados, new_dst_addr)
+            self.ack_no += len(dados)
+            pass
 
     def fechar(self):
         """
